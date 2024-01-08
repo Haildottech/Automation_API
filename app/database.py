dfaa5ff6959b0fd97a27ee5e280738e52041a305
saim2001 <saimrao49@gmail.com>
2024-01-08 10:10:18 +0500

    latest

--- app/database.py
+++ app/database.py
@@ -1,30 +1,15 @@
-from sqlalchemy import create_engine,MetaData
+from sqlalchemy import create_engine,MetaData,Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,Session
 from sqlalchemy.ext.automap import automap_base
 
-SQL_ALCHEMY_URL = "cockroachdb://saim:R2_RkmUt3xc59Gjzuhn33A@joking-egret-7111.8nk.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
+SQL_ALCHEMY_URL = "cockroachdb://saim:R2_RkmUt3xc59Gjzuhn33A@joking-egret-7111.8nk.cockroachlabs.cloud:26257/defaultdb?sslmode=require"
 
 engine = create_engine(SQL_ALCHEMY_URL)
-
-
-
 sessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+metadata = MetaData()
 
 baseclass = declarative_base()
-
-
-metadata = MetaData()
-from sqlalchemy import Table, MetaData
-
-Base = automap_base(metadata=metadata)
-Base.prepare()
-
-# Access the mapped classes
-Table1 = print(dir(Base))
-# Table2 = Base.classes.DMC_success_records
-
-metadata = MetaData()
 
 # Reflect each table separately
 table1 = Table('DMC_failed_records', metadata, autoload_with=engine)
